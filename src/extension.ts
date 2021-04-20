5c0ba4d00655599cc3a2fbcf0dc1b706b82feda4
暮鼓晨钟 <>
2021-04-20 11:53:51 +0800

    code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { getClipboardText, handleError, json2tscode, parseJson, pasteToMarker, validateLength } from './lib';
+import { getClipboardText, handleError, json2Columns, parseJson, pasteToMarker, validateLength } from './lib';
 const path = require('path');
 const os = require('os');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "json2tscode" is now active!');
 
-  context.subscriptions.push(vscode.commands.registerCommand('json2tscode.fromClipboard', transformFromClipboard));
+  context.subscriptions.push(vscode.commands.registerCommand('json2tscode.clipboard2columns', transformClipboard2columns));
 }
 
-function transformFromClipboard() {
+function transformClipboard2columns() {
   getClipboardText()
     .then(validateLength)
     .then(parseJson)
-    .then(json2tscode)
+    .then(json2Columns)
     .then((interfaces) => {
       pasteToMarker(interfaces);
     })
--- src/lib.ts
+++ src/lib.ts
@@ -20,9 +20,9 @@ export const validateLength = (text: any) => {
 export function parseJson(json: string): Promise<object> {  
   const tryEval = (str: any) => eval(`const a = ${str}; a`);
   // 下划线转驼峰
-  try {
-    return Promise.resolve(camelizeKeys(JSON.parse(json)));
-  } catch (ignored) {}
+  // try {
+  //   return Promise.resolve(camelizeKeys(JSON.parse(json)));
+  // } catch (ignored) {}
 
   try {
     return Promise.resolve(tryEval(json));
@@ -30,11 +30,18 @@ export function parseJson(json: string): Promise<object> {
     return Promise.reject(new Error("JSON 格式 无效"));
   }
 }
-export function json2tscode(json: Object): string {
+export function json2Columns(json: Object): string {
   const arr = [];
   for (const key in json) {
-    arr.push(
-    `{\n\tdataIndex: '${key}',\n\ttitle: intl.get('${key}')\n}`);
+    const keyStr = key.split('_');
+    console.log(keyStr);
+    if (keyStr[0] === "f"){
+      arr.push(
+        `{\n\tid: '${keyStr[keyStr.length-1]}',\n\tlabel: intl.get('${key}'),\n\t_node: <Input />,\n}`
+      );
+    } else if (keyStr[0] === "c") {
+      arr.push(`{\n\tdataIndex: '${keyStr[keyStr.length-1]}',\n\ttitle: intl.get('${key}')\n}`);
+    }
   }
   return arr.reduce((a, b) => `${a},\n${b}`);
 }
